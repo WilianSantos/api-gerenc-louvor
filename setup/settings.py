0bd8a83e5e70de883e35a9366731014c8a7cc688
Wilian <wilian.santos3@outlook.com>
2024-12-15 17:50:32 -0300

    Conexão do BD concluida

--- setup/settings.py
+++ setup/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+# Importe para acessar os arquivos entre as pastas do sitemas
+import os
+
+# Importação e carregamento do arquivo env do projeto
+from dotenv import load_dotenv
+load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_r=7-f7xw(we7w)f&9nb@%@m^*e^^6@-*93h_r4cgilxq0po*u'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,8 +84,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'praise_management_api',
+        "USER": "root",
+        "PASSWORD": str(os.getenv('PASSWORD_MYSQL')),
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 

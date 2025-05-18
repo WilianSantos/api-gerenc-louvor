128d8a1ea94159a2f217debdcf4b2f30ff1fdaa8
Wilian <wilian.santos3@outlook.com>
2025-05-18 15:45:53 -0300

    Corereção da rota send-email

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
 echo "📦 Instalando dependências..."
 pip install -r requirements.txt
 
-echo "🧨 Resetando banco de dados MySQL..."
-python manage.py shell < reset_db.py
+if [ "$RESET_DB" = "true" ]; then
+  echo "🧨 Resetando banco de dados..."
+  python manage.py shell < reset_db.py
+fi
 
 echo "🔧 Aplicando migrações..."
 python manage.py migrate --noinput
@@ -12,4 +16,3 @@ python manage.py collectstatic --noinput
 
 echo "🧙 Criando superusuário se necessário..."
 python manage.py shell < create_superuser.py
-
